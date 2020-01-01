1d858da30492afe2de09eab8432ccedc0fa404bb
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2020-01-01 21:07:05 +0900

    Remove: unneeded views

--- verdog/src/App.tsx
+++ verdog/src/App.tsx
@@ -5,31 +5,12 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/docs">
+            <Docs />
           </Route>
           <Route path="/">
-            <Home />
+            <Top />
           </Route>
         </Switch>
       </div>
@@ -37,14 +18,10 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+function Top() {
+  return <h2>Top</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
+function Docs() {
+  return <h2>Docs</h2>;
 }
